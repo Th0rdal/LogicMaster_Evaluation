99c4884c8b230c144e517124b2e505e1cfa296ee
patrick <thordal.simetzberger@gmail.com>
2024-12-14 14:18:16 +0100

    added clean batch file

---
+++ clean.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: Set the base path here
+set "base_path_log=C:\Users\patrick\PycharmProjects\LogicMaster_Evaluation"
+
+:: Folders to clean (relative to the base path)
+set folders=("logs\preprocessing" "logs\ai\ddpg" "logs\ai\ppo" "logs\ai\qlearning" "resources\models\qlearning" "resources\models\ppo" "resources\models\ddpg")
+
+:: Iterate through each folder and delete its files
+for %%f in %folders% do (
+    set "folder_path=%base_path%\%%~f"
+
+    if exist "%%~f" (
+        echo Cleaning folder: %%~f
+        del /q "%%~f\*.*"
+        echo Folder %%~f cleaned.
+    ) else (
+        echo Folder does not exist: %%~f
+    )
+)
+pause
